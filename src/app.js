edb6727096269b64fc4657b6a2c153eed9a5ee6e
mohdamircode <amir113918amir@gmail.com>
2026-02-10 21:35:52 -0800

    jwt and cookies

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ const app = express();
 const User = require("./modals/user");
 const { validateSignUpData } = require("./utils/validation")
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+app.use(cookieParser());
+
 
 /*app.use("/hello/2", (req,res) => {
     res.send("Abracadarab");
@@ -152,8 +158,6 @@ app.use("/", (err, req, res, next) => {
     }
 });*/
 
-app.use(express.json());
-
 app.post("/signup", async (req,res) => {
     /*const userObj = {
         firstName : "Aman",
@@ -216,6 +220,14 @@ app.post("/login", async (req,res) => {
         // const isPasswordValid = await bcrypt("Punjab#123","$2b$10$KoKxZAlPsTYTTIpymQnh6eXxJ6K8aEPt/p7514EcS6fhYk06hvnvy");
         const isPasswordValid = await bcrypt.compare(password,user.password);
         if(isPasswordValid){
+            // create a JWT Token
+            const token = await jwt.sign({ _id: user._id },"DEV@Tinder$790");
+            // console.log(token);
+
+            //Add the token to cookie and send the response back to the user
+
+            // res.cookie("token","jhjsuehjhvxqibuijhassjhx");
+            res.cookie("token", token);
             res.send("Login Successfull!!!");
         }
         else {
@@ -225,7 +237,35 @@ app.post("/login", async (req,res) => {
     } catch (err) {
         res.status(400).send("ERROR : " + err.message);
     }
-})
+});
+
+app.get("/profile", async (req,res) => {
+    try {const cookies = req.cookies;
+
+    const {token} = cookies;
+    // validate my token
+    if(!token) {
+        throw new Error("Invalid Token");
+    }
+
+    const decodedMessage = await jwt.verify(token, "DEV@Tinder$790");
+    // console.log(decodedMessage);
+
+    const { _id } = decodedMessage;
+    // console.log("Logged In User is : " + _id);
+
+    const user = await User.findById(_id);
+    if(!user){
+        throw new Error("User does not exist");
+    }
+
+    // console.log(cookies);
+    // res.send("Reading Cookie");
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
 
 //get user by email
 app.get("/user", async (req,res) => {
