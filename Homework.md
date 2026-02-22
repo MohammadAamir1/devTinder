af9fb8b3bf38db28f9697711e6bf1c7c8f597594
mohdamircode <amir113918amir@gmail.com>
2026-02-21 21:18:46 -0800

    feed + pagination

--- Homework.md
+++ Homework.md
@@ -118,3 +118,12 @@
 - Read about ref and populate https://mongoosejs.com/docs/populate.html
 - Cretae GET /user/requests/received with all the checks
 - Create GET GET /user/connections
+
+- logic for GET /feed API
+- Explore the $nin, $and, &ne and other query operator
+- Pagination
+
+/feed?page=1&limit=10 => 1-10 => .skip(0) & .limit(10)
+/feed?page=2&limit=10 => 11-20 => .skip(10) & .limit(10)
+/feed?page=3&limit=10 => 21-30 => .skip(20) & .limit(10)
+skip = ( page - 1) * limit;
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 
 const  { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../modals/connectionRequest");
+const User = require("../modals/user");
 
 const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
@@ -55,6 +56,54 @@ userRouter.get("/user/connections", userAuth, async (req,res) => {
     } catch(err) {
         res.status(400).send({ message: err.meassge });
     }
-})
+});
+
+userRouter.get("/feed", userAuth, async (req,res) => {
+// userRouter.get("/feed?page=1&limit=10", userAuth, async (req,res) => {
+    try{
+        // user should see all user cards except
+        //0. his own card
+        // 1. his connections
+        // 2. ignored people
+        // 3. already sent the connectioon request
+        const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+
+        // find all connection requets (sent + received)
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [{ fromUserId: loggedInUser._id}, { toUserId: loggedInUser._id }],
+        })
+        .select("fromUserId toUserId")
+        // .populate("fromUserId", "firstName")
+        // .populate("toUserId", "firstName");
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach((req) => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        });
+        // console.log(hideUsersFromFeed);
+
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUsersFromFeed)}},
+                { _id: { $ne: loggedInUser._id }},
+            ],
+        })
+        .select(USER_SAFE_DATA)
+        .skip(skip)
+        .limit(limit);
+
+        res.json({ data:users });
+        // res.send(users);
+        // res.send(connectionRequests);
+    } 
+    catch(err){
+        res.status(400).json({ message: err.message });
+    }
+});
 
 module.exports = userRouter;
